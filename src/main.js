735fc6069d531c9b3c2e76ed55e28a4f33904283
Vanshika Rana <vanshikarana@Vanshikas-MacBook-Pro.local>
2023-01-07 18:48:01 +0530

    debugging removed

--- src/main.js
+++ src/main.js
@@ -4,14 +4,13 @@ export default new Phaser.Game({
 	type: Phaser.AUTO,
 	width: 480,
 	height: 640,
-    scene: Game,
-    physics:{
-        default:'arcade',
-        arcade:{
-            gravity:{
-                y:200,
-            },
-            debug:true,
-        }
-    }
+	scene: Game,
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: {
+				y: 200,
+			},
+		},
+	},
 });
